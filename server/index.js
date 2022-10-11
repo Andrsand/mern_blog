8f9d33ddd630d0aad0c72ebbade6584defa5f585
andrsand <angorko@list.ru>
2022-10-11 09:45:30 +0300

    feature: Setting up a connection to mongoDB

--- server/index.js
+++ server/index.js
@@ -1,12 +1,25 @@
 // входной файл сервера
 import express from "express";
 import mongoose from "mongoose";
+import dotenv from 'dotenv'
 
 const app = express() // создано приложение app
+dotenv.config()
+
+//Constants
+const PORT = process.env.PORT || 3001
+const DB_USER = process.env.DB_USER
+const DB_PASSWORD = process.env.DB_PASSWORD
+const DB_NAME = process.env.DB_NAME
 
 async function start() {
     try {
-        await mongoose.connect()
+        await mongoose.connect(
+            //`mongodb + srv://${DB_USER}:${DB_PASSWORD}@cluster0.oxtaswn.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`,
+            "mongodb+srv://Andrey:1234_Andrey@cluster0.oxtaswn.mongodb.net/mern_blog?retryWrites=true&w=majority"
+        )
+
+        app.listen(PORT, () => console.log(`Server started on port: ${PORT}`))
     } catch (error) {
         console.log(error)
     }
