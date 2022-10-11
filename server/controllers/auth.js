f5810070f173e3b4761fefff25eb1c2ed4ff8bad
andrsand <angorko@list.ru>
2022-10-11 18:49:40 +0300

    feature: Authorization logic
    * Creation of authorization logic on the backend

---
+++ server/controllers/auth.js
@@ -0,0 +1,96 @@
+import User from '../models/User.js'
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+
+// Register user
+export const register = async (req, res) => {
+    try {
+        const { username, password } = req.body // данные приходящие с фронтенда
+
+        const isUsed = await User.findOne({ username }) // ищем в базе пользователя по username
+
+        if (isUsed) {                                   // если такой пользователь уже есть, завершаем регистрацию и отправляем сообщение
+            return res.json({
+                message: 'Данный username уже занят.'
+            })
+        }
+
+        const salt = bcrypt.genSaltSync(10) // сложность пароля
+        const hash = bcrypt.hashSync(password, salt) // хешируем пароль с определенной сложностью
+
+        const newUser = new User({ // новый пользователь с захешированным пролем
+            username,
+            password: hash,
+        })
+
+        await newUser.save() // сохранение нового пользователя в базц данных
+
+        res.json({
+            newUser,
+            message: 'Регистрация прошла успешно!', // отправляем сообщение на фронтенд
+        })
+    } catch (error) {
+        res.json({ message: 'Ошибка при создании пользователя.' })
+    }
+}
+// Login user
+export const login = async (req, res) => {
+    try {
+        const { username, password } = req.body
+        const user = await User.findOne({ username })
+        if (!user) {
+            return res.json({
+                message: 'Такого юзера не существует.'
+            })
+        }
+
+        const isPasswordCorrect = await bcrypt.compare(password, user.password) // сравнение с захешированным паролем
+
+        if (!isPasswordCorrect) {
+            return res.json({
+                message: 'Неверный пароль.'
+            })
+        }
+
+        const token = jwt.sign({
+            id: user._id,
+        },
+            process.env.JWT_SECRET,
+            { expiresIn: '30d' },
+        )
+
+        res.json({
+            token, user, message: 'Вы вошли в систему.',
+        })
+
+    } catch (error) {
+        res.json({ message: 'Ошибка при авторизации.' })
+    }
+}
+// Get Me
+export const getMe = async (req, res) => {
+    try {
+        const user = await User.findById(req.userId)
+
+        if (!user) {
+            return res.json({
+                message: 'Такого юзера не существует.'
+            })
+        }
+
+        const token = jwt.sign({
+            id: user._id,
+        },
+            process.env.JWT_SECRET,
+            { expiresIn: '30d' },
+        )
+
+        res.json({
+            user,
+            token,
+        })
+
+    } catch (error) {
+        res.json({ message: 'Нет доступа.' })
+    }
+}
--- server/index.js
+++ server/index.js
@@ -2,6 +2,9 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from 'dotenv'
+import cors from 'cors'
+
+import authRoute from './routes/auth.js'
 
 const app = express() // создано приложение app
 dotenv.config()
@@ -12,9 +15,17 @@ const DB_USER = process.env.DB_USER
 const DB_PASSWORD = process.env.DB_PASSWORD
 const DB_NAME = process.env.DB_NAME
 
+// Middleware
+app.use(cors()) // позволяет обращаться к bakend с разных ip адресов
+app.use(express.json()) // данные будут приходить в формате json
+
+// Routes
+app.use('/api/auth', authRoute)
+
 async function start() {
     try {
         await mongoose.connect(
+            //"mongodb + srv://Andrey:<password>@cluster0.oxtaswn.mongodb.net/?retryWrites=true&w=majority"
             //`mongodb + srv://${DB_USER}:${DB_PASSWORD}@cluster0.oxtaswn.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`,
             "mongodb+srv://Andrey:1234_Andrey@cluster0.oxtaswn.mongodb.net/mern_blog?retryWrites=true&w=majority"
         )
