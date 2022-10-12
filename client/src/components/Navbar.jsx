06b29444eb698fa23f85f0cfef8b953f57a0fa1e
andrsand <angorko@list.ru>
2022-10-12 09:15:17 +0300

    Navbar
    * Added markup to Navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,8 +2,35 @@ import React from 'react'
 
 export const Navbar = () => {
     return (
-        <div>
-            Navbar
+        <div className='flex py-4 justify-between items-center'>
+            <span className='flex justify-center items-center w-6 h-6 bg-gray-600 text-xs text-white rounded-sm'>
+                E
+            </span>
+            <ul className="flex gap-8">
+                <li>
+                    <a
+                        href='/'
+                        className="text-xs text-gray-400 hover:text-white"
+                    >Главная</a>
+                </li>
+                <li>
+                    <a
+                        href='/'
+                        className="text-xs text-gray-400 hover:text-white"
+                    >Мои посты</a>
+                </li>
+                <li>
+                    <a
+                        href='/'
+                        className="text-xs text-gray-400 hover:text-white"
+                    >Добавить пост</a>
+                </li>
+            </ul>
+
+            <div className='flex justify-center items-center bg-gray-600 text-xs text-white rounded-sm px-4 py-2'>
+                <button>Войти</button>
+            </div>
+
         </div>
     )
 }
