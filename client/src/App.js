2375aaf2833521a4827a38cff9b3f68555da7382
andrsand <angorko@list.ru>
2022-10-12 20:23:45 +0300

    feature: Handling Registration Errors
    * Installing react-toastify in the client

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,8 @@ import { AddPostPage } from './pages/AddPostPage'
 import { RegisterPage } from './pages/RegisterPage'
 import { LoginPage } from './pages/LoginPage'
 import { EditPostPage } from './pages/EditPostPage'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 function App() {
   return (
@@ -21,6 +23,8 @@ function App() {
         <Route path="register" element={<RegisterPage />} />
         <Route path="login" element={<LoginPage />} />
       </Routes>
+
+      <ToastContainer position="bottom-right" />
     </Layout>
   )
 }
