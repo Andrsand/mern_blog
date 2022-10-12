f1ae31d441001e97b55547dc446bcff01ffcca33
andrsand <angorko@list.ru>
2022-10-12 22:11:15 +0300

    feature: Entry and exit implementation

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
-import { registerUser } from '../redux/features/auth/authSlice'
+import { registerUser, checkIsAuth } from '../redux/features/auth/authSlice'
 import { toast } from 'react-toastify'
 
 
@@ -9,13 +9,17 @@ export const RegisterPage = () => {
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
     const { status } = useSelector((state) => state.auth)
+    const isAuth = useSelector(checkIsAuth)
+
+    const navigate = useNavigate()
     const dispatch = useDispatch()
 
     useEffect(() => {
         if (status) {
             toast(status)
         }
-    }, [status])
+        if (isAuth) navigate('/')
+    }, [status, isAuth, navigate])
 
     const handleSubmit = () => {
         try {
