3154c8b59e310f8a1b3e11f4a471f4394b8c8aae
andrsand <angorko@list.ru>
2022-10-14 09:28:14 +0300

    feature: Components
    * Installation npm install react-icons --save /client
    * Markup for displaying posts

--- client/src/pages/MainPage.jsx
+++ client/src/pages/MainPage.jsx
@@ -1,17 +1,25 @@
 import React from 'react'
+import { PostItem } from '../components/Postitem'
+import { PopularPosts } from '../components/PopularPosts'
 
 export const MainPage = () => {
     return (
         <div className='max-w-[900px] mx-auto py-10'>
             <div className='flex justify-between gap-8'>
                 <div className='flex flex-col gap-10 basis-4/5'>
-                    POSTS
+                    <PostItem />
+                    <PostItem />
+                    <PostItem />
+                    <PostItem />
                 </div>
                 <div className='basis-1/5'>
                     <div className='text-xs uppercase text-white'>
                         Популярное:
                     </div>
-                    POPULAR POSTS
+                    <PopularPosts />
+                    <PopularPosts />
+                    <PopularPosts />
+                    <PopularPosts />
                 </div>
             </div>
         </div>
