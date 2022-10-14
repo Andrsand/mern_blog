14287818c68ff76425a019c0f2a5ae9c999c6565
andrsand <angorko@list.ru>
2022-10-14 16:19:18 +0300

    feature: Receiving posts
    * Retrieving posts from the database

--- client/src/components/PopularPosts.jsx
+++ client/src/components/PopularPosts.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-export const PopularPosts = () => {
+export const PopularPosts = (post) => {
     return (
         <div className='bg-gray-600 my-1'>
             <div className='flex text-xs text-grey-300 p-2 hover:bg-gray-800 hover:text-white'>
-                Post popular
+                {post.title}
             </div>
         </div>
     )
